92b1d4c46a4744da4a57a8c035b5d4f731175016
energyBox2022 <107883564+energyBox2022@users.noreply.github.com>
2022-08-03 15:38:57 -0300

    getElementsByTagName

--- video24/javascript.js
+++ video24/javascript.js
@@ -1,7 +1,10 @@
 
-var cxt = document.getElementById("cxtexto").value="energybox2022";
+var cxt = document.getElementById('cxtexto').value='energyBox2022';
 
 document.write(cxt + "<br>");
 
 alert(cxt);
 
+/*A tag p não trabalha com value*/
+
+var texto = document.getElementById('texto').innerHTML='energyBox2022';
---
+++ video25/javascript.js
@@ -0,0 +1,5 @@
+
+
+let tagsP=document.getElementsByTagName("p")[2];
+
+document.write(tagsP.innerHTML);
